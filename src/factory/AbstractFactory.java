54c0856ab1cfd0b5245fa1a2e98ee5f521a9d3d2
escaleno <lezcano.jonatan@gmail.com>
2015-06-08 01:57:31 -0300

    Puse abstracto el getUnidad de AbstractFactory y correcciones menores de cosas comentadas.

--- src/factory/AbstractFactory.java
+++ src/factory/AbstractFactory.java
@@ -8,11 +8,10 @@ import factory.unidades.Unidad;
 
 public abstract class AbstractFactory {
 
-//	public abstract Unidad getUnidad(TipoUnidad unidadRequerida) throws UnidadInvalidaException;
+
 //	public abstract Construccion getConstruccion(TipoConstruccion construccionRequerida);
-	public Unidad getUnidad(TipoUnidad unidadRequerida,Posicion posicion)
-			throws UnidadInvalidaException, FueraDeRangoException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+	
+	public abstract Unidad getUnidad(TipoUnidad unidadRequerida,Posicion posicion)
+			throws UnidadInvalidaException, FueraDeRangoException;
 }
--- src/factory/ConstruccionFactory.java
+++ src/factory/ConstruccionFactory.java
@@ -1,8 +1,22 @@
 package factory;
 
+import common.Posicion;
+
+import exceptions.FueraDeRangoException;
+import exceptions.UnidadInvalidaException;
+import factory.unidades.TipoUnidad;
+import factory.unidades.Unidad;
+
 
 
 public class ConstruccionFactory extends AbstractFactory {
+
+	@Override
+	public Unidad getUnidad(TipoUnidad unidadRequerida, Posicion posicion)
+			throws UnidadInvalidaException, FueraDeRangoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 //	@Override
 //	public Unidad getUnidad(TipoUnidad unidadRequerida) {
